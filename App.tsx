f7c2e2a605c5996b0064ab074020ea02aac0e5e8
Rannaghar <156028870+chandan007-max@users.noreply.github.com>
2026-03-08 05:37:18 +0530

    Add App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppCore from './AppCore';
+
+const App: React.FC = () => {
+  return (
+    <div className="w-full h-[100dvh] overflow-hidden bg-gray-50 font-sans">
+      <AppCore />
+    </div>
+  );
+};
+
+export default App;
